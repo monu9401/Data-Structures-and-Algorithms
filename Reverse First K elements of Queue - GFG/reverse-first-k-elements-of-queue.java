8c12ae55b6420a66a5817236aedf7f068bd342f6
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-13 19:39:46 +0530

    Added solution - LeetHub

--- Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.java
+++ Reverse First K elements of Queue - GFG/reverse-first-k-elements-of-queue.java
@@ -55,20 +55,22 @@ class GfG {
     // Function to reverse first k elements of a queue.
     public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
         // add code here.
-        Stack<Integer> stack = new Stack<>();
-        for(int i=1;i<=k;i++)
-        {
-            stack.push(q.poll());
-        }
-        Queue<Integer> queue = new LinkedList<>();
-        while(!stack.isEmpty())
-        {
-            queue.offer(stack.pop());
-        }
-        while(!q.isEmpty())
-        {
-            queue.offer(q.poll());
-        }
-        return queue;
+       Stack<Integer> stack = new Stack<>();
+       int c=0;
+       while(c<k)
+       {
+           stack.push(q.poll());
+           c++;
+       }
+       Queue<Integer> queue = new LinkedList<>();
+       while(!stack.isEmpty())
+       {
+           queue.offer(stack.pop());
+       }
+       while(!q.isEmpty())
+       {
+           queue.offer(q.poll());
+       }
+       return queue;
     }
 }
