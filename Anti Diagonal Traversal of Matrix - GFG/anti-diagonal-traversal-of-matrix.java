c0ae8e9150099652550cf5380ac1e69f11caa122
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-20 21:20:54 +0530

    Added solution - LeetHub

---
+++ Anti Diagonal Traversal of Matrix - GFG/anti-diagonal-traversal-of-matrix.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            
+            String input_line[] = br.readLine().trim().split("\\s+");
+            int[][] matrix = new int[n][n];
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = Integer.parseInt(input_line[i * n + j]); 
+                }
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.antiDiagonalPattern(matrix);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+        int arr[]=new int[matrix.length*matrix[0].length];
+        int count=0;
+        for(int j=0;j<matrix.length;j++)
+        {
+            for(int i=0;i<=j;i++)
+            {
+                arr[count]=matrix[i][j-i];
+                count++;
+            }
+        }
+        for(int i=1;i<matrix.length;i++)
+        {
+            int x=i;
+            for(int j=matrix.length-1;j>=i;j--)
+            {
+                arr[count]=matrix[x][j];
+                x++;
+                count++;
+            }
+        }
+        return arr;
+    }
+}
