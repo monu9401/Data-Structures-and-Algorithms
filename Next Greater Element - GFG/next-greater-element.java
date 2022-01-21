8bcc71b818951b1d8aebce2b6798997c971aa846
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-21 23:37:50 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    long[] res = new Solution().nextLargerElement(arr, n);
+		    for (int i = 0; i < n; i++) 
+		        System.out.print(res[i] + " ");
+		    System.out.println();
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        long res[]=new long[n];
+        for(int i=1;i<n;i++)
+        {
+            while(!stack.isEmpty() && arr[stack.peek()]<arr[i])
+            {
+                res[stack.pop()]=arr[i];
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty())
+        {
+            res[stack.pop()]=-1;
+        }
+        return res;
+    } 
+}
