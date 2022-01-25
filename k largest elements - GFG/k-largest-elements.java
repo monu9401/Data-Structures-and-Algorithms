6d6989f9a2bcd7979f664609052656cfe068e867
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-25 22:47:50 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<n;i++)
+        {
+            if(pq.size()<k)
+            pq.offer(arr[i]);
+            else
+            {
+                if(pq.peek()<arr[i])
+                {
+                    pq.poll();
+                    pq.offer(arr[i]);
+                }
+            }
+        }
+        int arr1[]=new int[k];
+        int idx=k-1;
+        while(!pq.isEmpty())
+        {
+            arr1[idx]=pq.poll();
+            idx--;
+        }
+        return arr1;
+    }
+}
