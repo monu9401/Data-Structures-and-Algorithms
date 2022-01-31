b390445e766636aa33c75816dca01cea4aaf9843
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-31 10:48:44 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -1,56 +1,59 @@
 // { Driver Code Starts
-import java.io.*;
+//Initial Template for Java
+
 import java.util.*;
+import java.io.*;
+import java.io.*;
 
-class GFG {
-    // Driver code
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(inputLine[0]);
-            int m = Integer.parseInt(inputLine[1]);
-            int arr1[] = new int[n];
-            int arr2[] = new int[m];
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr1[i] = Integer.parseInt(inputLine[i]);
-            }
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < m; i++) {
-                arr2[i] = Integer.parseInt(inputLine[i]);
-            }
+public class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    long arr1[] = new long[n];
+		    long arr2[] = new long[m];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr1[i] = Long.parseLong(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<m; i++){
+		        arr2[i] = Long.parseLong(inputLine[i]);
+		    }
+		    Solution ob = new Solution();
+		    ob.merge(arr1, arr2, n, m);
+		    
+		    StringBuffer str = new StringBuffer();
+		    for(int i=0; i<n; i++){
+		        str.append(arr1[i]+" ");
+		    }
+		    for(int i=0; i<m; i++){
+		        str.append(arr2[i]+" ");
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+// } Driver Code Ends
 
-            new Solution().merge(arr1, arr2, n, m);
 
-            StringBuffer str = new StringBuffer();
-            for (int i = 0; i < n; i++) {
-                str.append(arr1[i] + " ");
-            }
-            for (int i = 0; i < m; i++) {
-                str.append(arr2[i] + " ");
-            }
-            System.out.println(str);
-        }
-    }
-}// } Driver Code Ends
+//User function Template for Java
 
-
-class Solution {
-
-    public void merge(int arr1[], int arr2[], int n, int m) {
-        
+class Solution
+{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
         int i=n-1,j=0;
-        while(i>=0 && j<m)
+        while(i>=0 && j<m && arr1[i]>arr2[j])
         {
-            if(arr1[i]>arr2[j])
-            {
-                int temp = arr1[i];
-                arr1[i]=arr2[j];
-                arr2[j]=temp;
-            }
+            long temp = arr1[i];
+            arr1[i] = arr2[j];
+            arr2[j] = temp;
             i--;
             j++;
         }
@@ -58,11 +61,3 @@ class Solution {
         Arrays.sort(arr2);
     }
 }
-
-/*
-
-0 3 5 7
-1 2 6 8 9
-
-
-*/
