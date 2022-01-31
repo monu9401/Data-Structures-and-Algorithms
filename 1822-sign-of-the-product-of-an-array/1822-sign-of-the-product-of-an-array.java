8471157e1ef7cab5ca0f0f92bec6cffaf403684a
Monu Nigam <monu.nigam.bit@gmail.com>
2022-01-31 21:39:55 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (7.54%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int negative=0;
+        for(int i:nums)
+        {
+            if(i==0)
+                return 0;
+            else if(i<0)
+                negative++;
+        }
+        if(negative%2==0)
+            return 1;
+        else return -1;
+    }
+}
